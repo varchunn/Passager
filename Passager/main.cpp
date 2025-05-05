edac0c790ec748b1c900b96fd601061b57e08eac
varchunn <varvaram186@gmail.com>
2025-05-05 22:47:11 +0300

    Initial Commit

---
+++ Passager/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Passager
+//
+//  Created by Варя Мінєнкова on 05.05.2025.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
