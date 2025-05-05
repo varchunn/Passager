c82f6ba4ea3b07d75baadeccf7ac59a1cac2eaaf
varchunn <varvaram186@gmail.com>
2025-05-05 22:48:00 +0300

    Update main.cpp

--- Passager/main.cpp
+++ Passager/main.cpp
@@ -1,14 +1,72 @@
-//
-//  main.cpp
-//  Passager
-//
-//  Created by Варя Мінєнкова on 05.05.2025.
-//
-
 #include <iostream>
+#include <vector>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+class House;
+class Factory;
+class Bank;
+
+class InsuranceAgentVisitor {
+public:
+    virtual void visit(House* house) = 0;
+    virtual void visit(Factory* factory) = 0;
+    virtual void visit(Bank* bank) = 0;
+    virtual ~InsuranceAgentVisitor() {}
+};
+
+class Visitable {
+public:
+    virtual void accept(InsuranceAgentVisitor* visitor) = 0;
+    virtual ~Visitable() {}
+};
+
+class House : public Visitable {
+public:
+    void accept(InsuranceAgentVisitor* visitor) override {
+        visitor->visit(this);
+    }
+};
+
+class Factory : public Visitable {
+public:
+    void accept(InsuranceAgentVisitor* visitor) override {
+        visitor->visit(this);
+    }
+};
+
+class Bank : public Visitable {
+public:
+    void accept(InsuranceAgentVisitor* visitor) override {
+        visitor->visit(this);
+    }
+};
+
+class InsuranceAgent : public InsuranceAgentVisitor {
+public:
+    void visit(House* house) override {
+        cout << "Страховий агент пропонує медичну страховку для сім’ї." << endl;
+    }
+
+    void visit(Factory* factory) override {
+        cout << "Страховий агент пропонує страховку від пожежі та повені для фабрики." << endl;
+    }
+
+    void visit(Bank* bank) override {
+        cout << "Страховий агент пропонує страховку від пограбування для банку." << endl;
+    }
+};
+
+int main() {
+    vector<Visitable*> locations = { new House(), new Factory(), new Bank() };
+    InsuranceAgent agent;
+
+    for (Visitable* place : locations) {
+        place->accept(&agent);
+    }
+
+    for (Visitable* place : locations) {
+        delete place;
+    }
+
     return 0;
 }
